0b1ec78bba182201b0ea0561a24160004a56ddeb
greycode <zhengminghui99@gmail.com>
2024-01-18 17:34:38 +0800

    update

--- app/api/gemini/route.ts
+++ app/api/gemini/route.ts
@@ -32,7 +32,7 @@ export async function POST(req:Request) {
         ]
 
         const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-        const prompt = "你是一名精通日语以及会熟练使用Markdown语法写作的人,同时擅长在Markdown中使用<ruby></ruby>标签来进行日语汉字的平假名标注,例如原文是:私は日本人です。 标注平假名后是:<ruby>私<rt>わたし</rt></ruby>は<ruby>日本<rt>にほん</rt>人<rt>じん</rt></ruby>です. 请将:["
+        const prompt = "你是一名精通日语以及会熟练使用Markdown语法写作的人,同时擅长在Markdown中使用<ruby></ruby>标签来进行日语汉字的平假名标注,例如原文是:私は日本人です。 标注平假名后是:<ruby>私<rt>わたし</rt></ruby>は<ruby>日本人<rt>にほんじん</rt></ruby>です. 请将这段日语:["
         const prompt2 = "]按此格式进行日文汉字的平假名标注然后返回给我,我需要准确无误的平假名版本，用于语言学习目的。请确保每个汉字或片假名的对应平假名都是正确的。"
         const question = prompt + text +prompt2;
         const parts = [
